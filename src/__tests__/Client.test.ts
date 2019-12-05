fb78ab3a0526882349f4bfd679b5ce31b17ba80a
serghei.masurceac <serghei.masurceac@titanium-soft.com>
2019-12-05 16:55:35 +0200

    Fix Client meta tests: rename meta argument

--- src/__tests__/Client.test.ts
+++ src/__tests__/Client.test.ts
@@ -268,8 +268,8 @@ test('Test client resource with meta', () => {
       }),
     client
       .post('/', {
-        onRequest: (body, meta) => {
-          expect(meta).toEqual(meta);
+        onRequest: (body, clientMeta) => {
+          expect(clientMeta).toEqual(meta);
           return body;
         },
       })({ meta, body: data })
@@ -278,8 +278,8 @@ test('Test client resource with meta', () => {
       }),
     client
       .post('/', {
-        onResponse: (body, meta) => {
-          expect(meta).toEqual(meta);
+        onResponse: (body, clientMeta) => {
+          expect(clientMeta).toEqual(meta);
           return body;
         },
       })({ meta, body: data })
